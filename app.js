bbd935880c8803f3d7b709aa5abcfb67323907a3
Rakai Seto <mamangrakai@gmail.com>
2022-10-01 17:32:20 +0700

    done ig

---
+++ app.js
@@ -0,0 +1,15 @@
+var styleSheet = document.createElement('style');
+function openForm() {
+  var styles = `
+    .wrapper > *:not(#exit) {filter: blur(2px);}
+  `;
+  styleSheet.innerHTML = styles;
+  document.head.appendChild(styleSheet);
+  document.getElementById('exit').style.display = 'block';
+}
+
+function closeForm() {
+  document.getElementById('exit').style.display = 'none';
+  styleSheet.innerHTML = ""; // you just need to reset the style as it was: blank
+  document.getElementsByTagName('head')[0].appendChild(styleSheet);
+}
